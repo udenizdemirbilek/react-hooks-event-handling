de27ed2e8dca97a171ca815dfb0d10fe276ccbda
demirbileku <udemirbilek2@gmail.com>
2021-06-14 14:14:40 +0300

    commit tree 0b11381faa9bba7c222dbadf5bb08753adc82cd6

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -2,7 +2,8 @@ import React from "react";
 
 function Tickler() {
   function tickle(event) {
-    console.log(event);
+    // console.log(event);
+    console.log("Teehee!");
   }
 
   return <button onClick={tickle}>Tickle me!</button>;
